9f783382893fe247b9118a763060b883c7f2dac8
Valentin Volkl <valentin.volkl@cern.ch>
2024-12-19 12:38:12 +0100

    up

--- cvmfs/duplex_curl.h
+++ cvmfs/duplex_curl.h
@@ -6,9 +6,9 @@
 #define CVMFS_DUPLEX_CURL_H_
 
 #ifdef _BUILT_IN_LIBCURL
-  #include "curl/curl.h" // abcdef
+  #include "curl/curl.h" // bcdef
 #else
-  #include <curl/curl.h> // abcdef
+  #include <curl/curl.h> // bcdef
 #endif
 
 #endif  // CVMFS_DUPLEX_CURL_H_
